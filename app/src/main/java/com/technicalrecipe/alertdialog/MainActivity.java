85de14ccdc6d48cf520aef365ca304cdca701268
Ajay Bhatt <ajay.bhatt@metsystechnologies.com>
2017-06-01 14:16:11 +0530

    3rd commit

--- app/src/main/java/com/technicalrecipe/alertdialog/MainActivity.java
+++ app/src/main/java/com/technicalrecipe/alertdialog/MainActivity.java
@@ -1,7 +1,11 @@
 package com.technicalrecipe.alertdialog;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void Dialog(View view) {
+        //Object of AlertDialogBuilder which is the inner class of AlertDialog
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+        alertDialogBuilder.setMessage("Are you sure, you want to do this?");
+
+        //Positive Button
+        alertDialogBuilder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Toast.makeText(MainActivity.this, "You clicked Yes button", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        //Negative Button
+        alertDialogBuilder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                finish();
+            }
+        });
+
+        //Open Alert Dialog
+        AlertDialog alertDialog = alertDialogBuilder.create();
+        alertDialog.show();
+
     }
 }
